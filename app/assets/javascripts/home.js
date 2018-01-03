4f3c78b92e61e67ec3428eefe26befc31c701d9e
bishosilwal <aayussilwal@gmail.com>
2018-01-03 15:12:15 +0545

    updated modal pdf viewer

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -9,3 +9,17 @@ function insRow(){
 	$("#rowcount").val(totalRow+1);
 	
 }
+
+
+$(document).ready(function($) {
+	$('#viewPdf').on('show.bs.modal', function (event) {
+	  var button = $(event.relatedTarget) // Button that triggered the modal
+	  var docId = button.data('id') // Extract info from data-* attributes
+	  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+	  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+	  var modal = $(this)
+	  modal.find('.modal-body #pdfObject').attr("data","/home/"+docId+".pdf")
+
+
+	})
+});
